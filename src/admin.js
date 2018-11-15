214efe82b2d47ba2e0ad32d5dec82039e9ca1b77
Vahe Pezeshkian <vahepezeshkian@wlan-205104.nbw.tue.nl>
2018-11-15 14:55:23 +0100

    Multiple questions

--- src/admin.js
+++ src/admin.js
@@ -9,20 +9,35 @@ export class Admin {
     getData().results = [];
   }
 
+  createPoll(target) {
+    target.innerHTML = `
+      <div>
+        <label for="question">Please write the question you are struggling with:</label><br/>
+        <input type="text" size="80" id="question"/><br/>
+
+        <label for="type">Poll Type:</label><br/>
+        <input type="text" size="30" id="type"/><br/>
+
+        <label for="options">Please specify options (comma-separated):</label><br/>
+        <input type="text" size="80" id="options"/><br/>
+
+        <hr/>
+      </div>
+    `;
+  }
+
   render() {
+
     this.element.innerHTML = `
-      <label for="question">Please write the question you are struggling with:</label><br/>
-      <input type="text" size="80" id="question"/><br/>
-
-      <label for="type">Poll Type:</label><br/>
-      <input type="text" size="30" id="type"/><br/>
-
-      <label for="options">Please specify options (comma-separated):</label><br/>
-      <input type="text" size="80" id="options"/><br/>
-
       <button id="new">Add Poll</button>
       <button id="start">Start</button>
+      <br/>
+      <div id="container"></div>
     `;
+
+    var div = document.createElement('div');
+    this.createPoll(div);
+    this.element.querySelector("#container").appendChild(div);
 
     this.element.querySelector("button[id='start']").addEventListener("click", ev => {
         // always add `preventDefault` in an event handler. otherwise, the browser
@@ -31,17 +46,28 @@ export class Admin {
         // since we have no server, we don't want that :-)
         ev.preventDefault();
 
-        let questionText = this.element.querySelector("#question").value;
-        let questionType = this.element.querySelector("#type").value;
-        let options = this.element.querySelector("#options").value.split(",");
+        let question = this.element.querySelector("#question").value;
+        let type = this.element.querySelector("#type").value;
+        let choice = this.element.querySelector("#options").value.split(",");
 
-        getData().polls.push({questionText,questionType, options});
+        getData().polls.push({question,type, choice});
 
         console.log(getData());
 
         new Poll(this.element).render("Nobi");
     });
 
+    this.element.querySelector("button[id='new']").addEventListener("click", ev => {
+        // always add `preventDefault` in an event handler. otherwise, the browser
+        // will do some default action which usually means submitting the data to the server,
+        // which causes the entire page to reload.
+        // since we have no server, we don't want that :-)
+        ev.preventDefault();
+
+        var div = document.createElement('div');
+        this.createPoll(div);
+        this.element.querySelector("#container").appendChild(div);
+    });
   }
 
 }
