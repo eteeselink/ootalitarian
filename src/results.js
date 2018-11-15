055bf5bdf19e75e31cdb13265d8713ca1154b97f
Hossain Muctadir <muctadir.dinar@gmail.com>
2018-11-15 15:29:12 +0100

    results showing

--- src/results.js
+++ src/results.js
@@ -2,7 +2,7 @@ import { getData } from './data.js';
 
 export class Results {
 
-    
+
     constructor(element) {
         console.log('result');
         this.element = element;
@@ -15,9 +15,33 @@ export class Results {
         console.log(this.data);
 
         for (var i = 0; i < this.data.results.length; i++) {
-            html += `<p>${this.data.polls[i]}</p>:<p>${this.data.results[i]}</p>`;
+            var answerCount = this.getAnswerCount(this.data.polls[i], this.data.results[i]);
+            var answerStr = '(';
+            Object.keys(answerCount).forEach(a => {
+                answerStr += `${a}:${answerCount[a]}`;
+            });
+            answerStr += ')';
+
+            html += `<p>${this.data.polls[i].question} : ${answerStr}</p>`;
         }
 
-       this.element.innerHTML = html;
+        this.element.innerHTML = html;
+    }
+
+    getAnswerCount(poll, answers) {
+        var result = {};
+        poll.choice.forEach(item => {
+            result[item] = this.countElement(answers, item);
+        });
+        return result;
+    }
+
+    countElement(array, item) {
+        var count = 0;
+        for (var i = 0; i < array.length; ++i) {
+            if (array[i] === item)
+                count++;
+        }
+        return count;
     }
 }
